6a73266ca8ecd8f05ac6a72807c7fd66b7e2e60e
Patric Ntazama <ntazamapat@yahoo.fr>
2019-05-15 12:39:30 +0200

    Added Sign In

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var signUp = document.querySelector('.nav__signup')
 var signForm= document.querySelector('.signup');
 var closeForm = document.querySelector('.signup__form__close');
 
+
 signUp.addEventListener('click',function(){
 
 signForm.style.display='block';
@@ -12,4 +13,24 @@ signForm.style.display='block';
 closeForm.addEventListener('click',function(){
     signForm.style.display="none";
 })
+
+
+// Sign In Form
+
+var signIn = document.querySelector(".nav__signin");
+var signinform=document.querySelector(".signIn");
+var signInClose = document.querySelector(".signin__close")
+
+signIn.addEventListener('click',function(){
+    console.log('I have clicked!!');
+    signinform.style.display = 'block';
+})
+
+signInClose.addEventListener('click',function(){
+    signinform.style.display="none";
+})
+
+
+
+
 
